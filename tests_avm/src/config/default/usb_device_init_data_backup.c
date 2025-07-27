21f48d9cbe6de32d26ca74dab3c3159880245ba5
Daniel Josué <daniel.cubillost@usm.cl>
2025-07-26 21:05:33 -0400

    borrar archivos innecesarios

--- tests_avm/src/config/default/usb_device_init_data_backup.c
+++
@@ -1,3 +0,0 @@
-// Backup del archivo original de descriptores USB
-// Este archivo contiene la configuración completa de descriptores CDC
-// Para restaurar: renombrar este archivo a usb_device_init_data.c
